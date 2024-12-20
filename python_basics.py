e783f238d6c41ee87632b8f8bb20fe439ed882b5
Namrata <namru009@gmail.com>
2024-12-20 08:17:59 +0545

    day 1 on working on python basics

---
+++ python_basics.py
@@ -0,0 +1,27 @@
+# ## showing o/p to user
+# print("Hello world!!")
+#
+# # #Taking input from user
+# # var1 = input("Enter a value:")
+# # print(var1)
+# #
+# # #Range function
+#
+# for i in range(1,20,5):
+#     print(i)
+#
+# ## comments
+#
+# """ I am testing
+# multiline comments"""
+#
+#
+#  ## string
+# string_var = "First string test"
+# print(string_var)
+
+# slicing
+variable = [1,20,3]
+print(variable)
+
+
